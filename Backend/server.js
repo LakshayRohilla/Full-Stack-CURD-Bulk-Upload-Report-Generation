724c53a192cd92bdd6d29411239ca086766e450e
Rohilla <lakshay.rohilla@sms-group.com>
2025-12-06 12:46:09 +0530

    added env

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use('/', dummyRoute);
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+// app.listen(5000, () => {
+//     console.log('Server is running on port 5000');
+// });
+
+app.listen(process.env.PORT || 5000);
