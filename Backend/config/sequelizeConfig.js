51853b90382fb1025b0cef4e9360a4f84287d5e1
Rohilla <lakshay.rohilla@sms-group.com>
2025-12-06 15:49:18 +0530

    added sequelize orm

---
+++ Backend/config/sequelizeConfig.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config({ debug: false });
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+});
+
+export default sequelize;
